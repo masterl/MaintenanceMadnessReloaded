5eed44f3eca07a12a5175581ccebfcfc11bfb03d
Leonardo de Oliveira Lourenço <bigodation@gmail.com>
2021-08-04 16:42:56 -0700

    Add constants.mod_folder

--- maintenance-madness-reloaded/util/constants.lua
+++ maintenance-madness-reloaded/util/constants.lua
@@ -4,5 +4,6 @@ local mod_name = 'maintenance-madness-reloaded'
 
 constants.mod_name = mod_name
 constants.mod_prefix = mod_name .. '-'
+constants.mod_folder = '__' .. mod_name .. '__/'
 
 return constants
