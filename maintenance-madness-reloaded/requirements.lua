1dddf9b89b6b98696c7528e8f16d7a96f3b3e6ca
Leonardo de Oliveira Lourenço <bigodation@gmail.com>
2021-08-04 12:46:27 -0700

    Add auto formatter

---
+++ maintenance-madness-reloaded/requirements.lua
@@ -0,0 +1,7 @@
+-- This is currently a dummy file just to keep track of dependencies
+return {
+    dev_dependencies = {
+        { name = 'luaformatter' },
+        { name = 'luacheck' },
+    }
+}
