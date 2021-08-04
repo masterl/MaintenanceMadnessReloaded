fe5b723127763f6af0aa054a05bec252d5d727a8
Leonardo de Oliveira Lourenço <bigodation@gmail.com>
2021-08-04 12:46:56 -0700

    Add automated test support with busted and luacov

--- maintenance-madness-reloaded/requirements.lua
+++ maintenance-madness-reloaded/requirements.lua
@@ -1,7 +1,13 @@
 -- This is currently a dummy file just to keep track of dependencies
 return {
     dev_dependencies = {
+        { name = 'busted' },
+        { name = 'luacov' },
         { name = 'luaformatter' },
         { name = 'luacheck' },
+        {
+            name = 'luacov-reporters',
+            link = 'https://raw.githubusercontent.com/tarantool/luacov-reporters/master/luacov-reporters-scm-1.rockspec'
+        }
     }
 }
