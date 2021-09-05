e183f3e7fab19fa6cf5cb03d7121f003d3fc160f
Leonardo de Oliveira Lourenço <bigodation@gmail.com>
2021-09-05 05:04:36 -0700

    Refactor conversions/time.lua

--- maintenance-madness-reloaded/util/conversions/time.lua
+++ maintenance-madness-reloaded/util/conversions/time.lua
@@ -5,21 +5,20 @@ local floor = math.floor
 local SECONDS_PER_HOUR = 3600
 local TICKS_PER_SECOND = 60
 
+local time = {}
+
 ---Convert time from hours to seconds.
 ---@param time_in_hours number
 ---@return number #the result of math.floor on the converted value
-local function hours_to_seconds( time_in_hours )
+function time.hours_to_seconds( time_in_hours )
     return floor( time_in_hours * SECONDS_PER_HOUR )
 end
 
 ---Convert a time in seconds to the corresponding amount of game ticks.
 ---@param time_in_seconds number
 ---@return number
-local function seconds_to_ticks( time_in_seconds )
+function time.seconds_to_ticks( time_in_seconds )
     return time_in_seconds * TICKS_PER_SECOND
 end
 
-return {
-    hours_to_seconds = hours_to_seconds,
-    seconds_to_ticks = seconds_to_ticks
-}
+return time
