257fcf54180c83b2db74af1363a36570bfc27249
Leonardo de Oliveira Lourenço <bigodation@gmail.com>
2021-09-05 07:09:01 -0700

    Add time.minutes_to_ticks

--- maintenance-madness-reloaded/spec/util/conversions/time_spec.lua
+++ maintenance-madness-reloaded/spec/util/conversions/time_spec.lua
@@ -1,21 +1,38 @@
 insulate( 'util.conversions.time', function()
     local time = require( 'util.conversions.time' )
 
+    local SECONDS_PER_MINUTE = 60
+    local SECONDS_PER_HOUR = 3600
+    local TICKS_PER_SECOND = 60
+    local TICKS_PER_MINUTE = SECONDS_PER_MINUTE * TICKS_PER_SECOND
+
     describe( 'time.hours_to_seconds', function()
         local hours_to_seconds = time.hours_to_seconds
 
         it( 'should correctly convert values from hours to seconds', function()
-            assert.are.equal( 3600, hours_to_seconds( 1 ) )
-            assert.are.equal( 1800, hours_to_seconds( 0.5 ) )
+            assert.are.equal( 1 * SECONDS_PER_HOUR, hours_to_seconds( 1 ) )
+            assert.are.equal( 0.5 * SECONDS_PER_HOUR, hours_to_seconds( 0.5 ) )
         end )
     end )
 
     describe( 'time.minutes_to_seconds', function()
         local minutes_to_seconds = time.minutes_to_seconds
 
-        it( 'should correctly convert values from hours to seconds', function()
-            assert.are.equal( 60, minutes_to_seconds( 1 ) )
-            assert.are.equal( 90, minutes_to_seconds( 1.5 ) )
+        it( 'should correctly convert values from minutes to seconds',
+            function()
+            assert.are.equal( 1 * SECONDS_PER_MINUTE, minutes_to_seconds( 1 ) )
+            assert.are.equal( 1.5 * SECONDS_PER_MINUTE,
+                              minutes_to_seconds( 1.5 ) )
+        end )
+    end )
+
+    describe( 'time.minutes_to_ticks', function()
+        local minutes_to_ticks = time.minutes_to_ticks
+
+        it( 'should correctly convert values from minutes to ticks', function()
+            assert.are.equal( 1 * TICKS_PER_MINUTE, minutes_to_ticks( 1 ) )
+            assert.are
+                .equal( 17.5 * TICKS_PER_MINUTE, minutes_to_ticks( 17.5 ) )
         end )
     end )
 
@@ -24,7 +41,6 @@ insulate( 'util.conversions.time', function()
 
         it( 'should correctly convert values from seconds to game ticks',
             function()
-            local TICKS_PER_SECOND = 60
 
             assert.are.equal( 45 * TICKS_PER_SECOND, seconds_to_ticks( 45 ) )
             assert.are.equal( 70 * TICKS_PER_SECOND, seconds_to_ticks( 70 ) )
--- maintenance-madness-reloaded/util/conversions/time.lua
+++ maintenance-madness-reloaded/util/conversions/time.lua
@@ -5,6 +5,7 @@ local floor = math.floor
 local SECONDS_PER_HOUR = 3600
 local SECONDS_PER_MINUTE = 60
 local TICKS_PER_SECOND = 60
+local TICKS_PER_MINUTE = SECONDS_PER_MINUTE * TICKS_PER_SECOND
 
 local time = {}
 
@@ -27,6 +28,13 @@ end
 ---@return number
 function time.seconds_to_ticks( time_in_seconds )
     return time_in_seconds * TICKS_PER_SECOND
+end
+
+---Convert a time in minutes to the corresponding amount of game ticks.
+---@param time_in_minutes number
+---@return number
+function time.minutes_to_ticks( time_in_minutes )
+    return time_in_minutes * TICKS_PER_MINUTE
 end
 
 return time
